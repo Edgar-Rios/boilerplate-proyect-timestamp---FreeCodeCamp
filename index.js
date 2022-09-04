908f7a4c7a008a91caf7729f6ad3271da5665ce1
Edgar-Rios <sistunla2020@gmail.com>
2022-09-04 00:27:23 -0300

    proyect ready

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({
+  optionsSuccessStatus: 200
+})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +23,38 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({
+    greeting: 'hello API'
+  });
 });
 
+/* MI CODIGO */ // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< MI CODIGO
 
+app.get('/api/:date?', (req, res) => {
+
+  try {
+    let { date } = req.params;
+    let parsedDate;
+
+    if (date)
+      parsedDate = (!isNaN(date)) ? new Date(+date) : new Date(date);
+    else
+      parsedDate = new Date();
+
+    if(parsedDate == "Invalid Date")
+      return res.json({error: "Invalid Date"});
+
+    res.json({
+      unix: parsedDate.getTime(),
+      utc: parsedDate.toUTCString(),
+    })
+  } catch(error) {
+    res.json(error)
+  }
+
+})
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^MI CODIGO
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
